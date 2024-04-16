581fe4edbe805e6ec3f38a77d4cf78a816a8aa6e
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-04-16 23:10:26 +0600

    updated

---
+++ 10_Source-code_030-030_Request-headers_add-meta-data-about-the-request/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$ch = curl_init();
+
+$headers = [
+    "Authorization: Client-ID "
+];
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers
+]);
+
+$response = curl_exec($ch);
+
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+echo $status_code, "\n";
+
+echo $response, "\n";
